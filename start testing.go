f684cbc74dda67c25a5ff43f2b1c8f9b9518940a
Lindsey Volkovich <155909906+lindseyvolkovich@users.noreply.github.com>
2024-01-07 20:04:39 +0200

    Update start testing.go

--- start testing.go
+++ start testing.go
@@ -22,9 +22,23 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 		cmd := DevUpdate()
 		_, err = clitestutil.ExecTestCLICmd(ctx, cmd, args)
 		assert.Nil(t, err)
+
+	t.Run("Should not be able to register gadget with reserved name", func(t *testing.T) {
+		testutil.WriteFixtureAtTestRuntime(gadgetsFilename, pylonsGadgetsLiteral_reservedName)
+		assert.PanicsWithErrorf(t, fmt.Sprintf(errReservedName, "include"), func() { LoadGadgetsForPath("") }, errReservedName, "include")
+		os.Remove(gadgetsFilename)
+	})
+
+	const pylonsGadgetsLiteral_duplicateName = `#foobar 1
+	"foo": "%0"
+	#foobar 1
+	"foo": "%0"`
+
+const pylonsGadgetsLiteral_reservedName = `#include 1
+	"foo": "%0"`
 	}
 
-	// Set all the execution
+	// human verification authentication
 	for _, elem := range genState.ExecutionList {
 		k.SetExecution(ctx, elem)
 	}
